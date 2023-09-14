c325b1b01d4e6d442397481c8f47bab3c88192fa
Tharuka Kasthuri Arachchige <tharuka.ckasthuri@gmail.com>
2023-09-14 18:08:55 +0200

    packaging

---
+++ support/kernel/setup.py
@@ -0,0 +1,16 @@
+"""Jaclang setup file."""
+
+from setuptools import find_packages, setup  # type: ignore
+
+VERSION = "0.0.1"
+
+setup(
+    name="jackernel",
+    version=VERSION,
+    packages=find_packages(include=["jackernel", "jackernel.*"]),
+    install_requires=["ipykernel==6.19.2", "pygments==2.10.0", "jaclang"],
+    package_data={
+        "": ["*.ini", "*.jac", ".py"],
+    },
+    url="https://github.com/Jaseci-Labs/jaclang/tree/main/support/kernel",
+)
