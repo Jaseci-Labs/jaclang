0f16e7513728eba386520562929fe5ce4eba96aa
Alexie (Boyong) Madolid <maex@tuta.io>
2024-01-27 00:37:15 +0800

    temporary

--- jaclang_fastapi/__init__.py
+++ jaclang_fastapi/__init__.py
@@ -2,7 +2,6 @@
 
 from jaclang_fastapi.core import app
 from jaclang_fastapi.core.decorators import (
-    fapi,
     jdelete,
     jget,
     jhead,
@@ -12,6 +11,7 @@ from jaclang_fastapi.core.decorators import (
     jtrace,
 )
 from jaclang_fastapi.core.runner import start
+
 
 __all__ = [
     "app",
@@ -23,5 +23,6 @@ __all__ = [
     "jhead",
     "joptions",
     "jtrace",
+    "jobj",
     "start",
 ]
