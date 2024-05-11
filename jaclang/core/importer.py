ed6a31e7833a006a1de80a6398c0e585126738e2
marsninja <jason@mars.ninja>
2024-05-11 17:21:48 -0400

    fix: fixing the py dynamic import resoution

--- jaclang/core/importer.py
+++ jaclang/core/importer.py
@@ -2,9 +2,9 @@
 
 import importlib
 import marshal
+import os
 import sys
 import types
-from os import getcwd, path
 from typing import Optional, Union
 
 from jaclang.compiler.absyntree import Module
@@ -26,11 +26,11 @@ def jac_importer(
     items: Optional[dict[str, Union[str, bool]]] = None,
 ) -> Optional[types.ModuleType]:
     """Core Import Process."""
-    dir_path, file_name = path.split(
-        path.join(*(target.split("."))) + (".jac" if lng == "jac" else ".py")
+    dir_path, file_name = os.path.split(
+        os.path.join(*(target.split("."))) + (".jac" if lng == "jac" else ".py")
     )
-    module_name = path.splitext(file_name)[0]
-    package_path = dir_path.replace(path.sep, ".")
+    module_name = os.path.splitext(file_name)[0]
+    package_path = dir_path.replace(os.path.sep, ".")
 
     if package_path and f"{package_path}.{module_name}" in sys.modules:
         return sys.modules[f"{package_path}.{module_name}"]
@@ -38,11 +38,15 @@ def jac_importer(
         return sys.modules[module_name]
 
     caller_dir = get_caller_dir(target, base_path, dir_path)
-    full_target = path.normpath(path.join(caller_dir, file_name))
+    full_target = os.path.normpath(os.path.join(caller_dir, file_name))
 
     if lng == "py":
         module = py_import(
-            target=target, items=items, absorb=absorb, mdl_alias=mdl_alias
+            target=target,
+            full_path=full_target,
+            items=items,
+            absorb=absorb,
+            mdl_alias=mdl_alias,
         )
     else:
         module_name = override_name if override_name else module_name
@@ -53,12 +57,12 @@ def jac_importer(
             codeobj = mod_bundle.mod_deps[full_target].gen.py_bytecode
             codeobj = marshal.loads(codeobj) if isinstance(codeobj, bytes) else None
         else:
-            gen_dir = path.join(caller_dir, Con.JAC_GEN_DIR)
-            pyc_file_path = path.join(gen_dir, module_name + ".jbc")
+            gen_dir = os.path.join(caller_dir, Con.JAC_GEN_DIR)
+            pyc_file_path = os.path.join(gen_dir, module_name + ".jbc")
             if (
                 cachable
-                and path.exists(pyc_file_path)
-                and path.getmtime(pyc_file_path) > path.getmtime(full_target)
+                and os.path.exists(pyc_file_path)
+                and os.path.getmtime(pyc_file_path) > os.path.getmtime(full_target)
             ):
                 with open(pyc_file_path, "rb") as f:
                     codeobj = marshal.load(f)
@@ -102,28 +106,37 @@ def create_jac_py_module(
 
 def get_caller_dir(target: str, base_path: str, dir_path: str) -> str:
     """Get the directory of the caller."""
-    caller_dir = base_path if path.isdir(base_path) else path.dirname(base_path)
-    caller_dir = caller_dir if caller_dir else getcwd()
+    caller_dir = base_path if os.path.isdir(base_path) else os.path.dirname(base_path)
+    caller_dir = caller_dir if caller_dir else os.getcwd()
     chomp_target = target
     if chomp_target.startswith("."):
         chomp_target = chomp_target[1:]
         while chomp_target.startswith("."):
-            caller_dir = path.dirname(caller_dir)
+            caller_dir = os.path.dirname(caller_dir)
             chomp_target = chomp_target[1:]
-    caller_dir = path.join(caller_dir, dir_path)
+    caller_dir = os.path.join(caller_dir, dir_path)
     return caller_dir
 
 
 def py_import(
     target: str,
+    full_path: str,
     items: Optional[dict[str, Union[str, bool]]] = None,
     absorb: bool = False,
     mdl_alias: Optional[str] = None,
 ) -> types.ModuleType:
     """Import a Python module."""
     try:
-        target = target.lstrip(".") if target.startswith("..") else target
-        imported_module = importlib.import_module(name=target)
+        level_up = (
+            0
+            if not target.startswith(".")
+            else len(target) - len(target.lstrip(".")) - 1
+        )
+        package_name = find_py_package_name(file_path=full_path, level_up=level_up)
+        print(f"Level up: {level_up}")
+        print(f"Full path: {full_path}")
+        print(f"Package name: {package_name}")
+        imported_module = importlib.import_module(name=target, package=package_name)
         main_module = __import__("__main__")
         if absorb:
             for name in dir(imported_module):
@@ -158,3 +171,19 @@ def py_import(
     except ImportError as e:
         print(f"Failed to import module {target}")
         raise e
+
+
+def find_py_package_name(file_path: str, level_up: int = 0) -> Optional[str]:
+    """Find the package name given an absolute path of a module file."""
+    for path in sys.path:
+        if file_path.startswith(path):
+            relative_path = os.path.relpath(file_path, path)
+            # Normalize the path and adjust levels up dynamically
+            current_directory = os.path.dirname(relative_path)
+            for _ in range(level_up):
+                current_directory = os.path.dirname(
+                    current_directory
+                )  # Go one level up iteratively
+            package_path = current_directory.replace(os.sep, ".")
+            return package_path
+    return None
