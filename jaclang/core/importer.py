e5d9142cdedf14273fa77e64cb8166e6f7636a41
Your Name <am028275@gmail.com>
2024-05-29 15:19:42 +0000

    trannforming sys.modules to loaded programs

--- jaclang/core/importer.py
+++ jaclang/core/importer.py
@@ -1,11 +1,11 @@
 """Special Imports for Jac Code."""
 
 import types
-from typing import Optional, Union
+from typing import Dict, Optional, Union
 
 from jaclang.compiler.absyntree import Module
 
-from .machine import machine
+from .machine import JacProgram, machine
 
 
 def jac_importer(
@@ -17,8 +17,8 @@ def jac_importer(
     override_name: Optional[str] = None,
     mod_bundle: Optional[Module] = None,
     lng: Optional[str] = "jac",
-    items: Optional[dict[str, Union[str, bool]]] = None,
-) -> Optional[types.ModuleType]:
+    items: Optional[Dict[str, Union[str, bool]]] = None,
+) -> Optional[Union[types.ModuleType, JacProgram]]:
     """Core Import Process."""
     program = machine.load_program(
         filename=target,
@@ -31,4 +31,4 @@ def jac_importer(
         absorb=absorb,
         mdl_alias=mdl_alias,
     )
-    return program.module
+    return program
--- jaclang/core/machine.py
+++ jaclang/core/machine.py
@@ -1,8 +1,7 @@
 """JAC Code for Import."""
 
-import importlib
+import importlib.util
 import marshal
-import sys
 import types
 from os import getcwd, path
 from typing import Dict, Optional, Union
@@ -25,13 +24,15 @@ class JacProgram:
         override_name: Optional[str] = None,
         mod_bundle: Optional[Module] = None,
         lng: Optional[str] = "jac",
-        items: Optional[dict[str, Union[str, bool]]] = None,
+        items: Optional[Dict[str, Union[str, bool]]] = None,
         absorb: bool = False,
         mdl_alias: Optional[str] = None,
+        jac_machine: Optional["JacMachine"] = None,
     ) -> None:
         """Initialize a JacProgram instance."""
         self.bytecode: Optional[bytes] = None
         self.module: Optional[types.ModuleType] = None
+        self.jac_machine = jac_machine
         self.load_program(
             filename,
             base_path,
@@ -52,7 +53,7 @@ class JacProgram:
         override_name: Optional[str],
         mod_bundle: Optional[Module],
         lng: Optional[str],
-        items: Optional[dict[str, Union[str, bool]]],
+        items: Optional[Dict[str, Union[str, bool]]],
         absorb: bool,
         mdl_alias: Optional[str],
     ) -> None:
@@ -75,6 +76,7 @@ class JacProgram:
                     items=items,
                     absorb=absorb,
                     mdl_alias=mdl_alias,
+                    jac_machine=self.jac_machine,
                 )
                 self.bytecode = b""
                 self.module = module
@@ -112,13 +114,21 @@ class JacProgram:
                 if not codeobj:
                     raise ImportError(f"No bytecode found for {full_target}")
                 module = create_jac_py_module(
-                    mod_bundle, module_name, package_path, full_target
+                    mod_bundle,
+                    module_name,
+                    package_path,
+                    full_target,
+                    jac_machine=self.jac_machine,
                 )
                 with sys_path_context(path.dirname(full_target)):
                     exec(codeobj, module.__dict__)
 
                 self.bytecode = codeobj
                 self.module = module
+
+                if self.jac_machine:
+                    self.jac_machine.loaded_programs[module_name] = module
+                    print(f"Jac program registered: {module_name}")
 
         except Exception as e:
             print(f"Failed to load program {module_name}: {e}")
@@ -150,23 +160,30 @@ class JacProgram:
 
 
 def create_jac_py_module(
-    mod_bundle: Optional[Module], module_name: str, package_path: str, full_target: str
+    mod_bundle: Optional[Module],
+    module_name: str,
+    package_path: str,
+    full_target: str,
+    jac_machine: Optional["JacMachine"],
 ) -> types.ModuleType:
     """Create a module."""
     module = types.ModuleType(module_name)
     module.__file__ = full_target
     module.__name__ = module_name
     module.__dict__["__jac_mod_bundle__"] = mod_bundle
-    if package_path:
-        parts = package_path.split(".")
-        for i in range(len(parts)):
-            package_name = ".".join(parts[: i + 1])
-            if package_name not in sys.modules:
-                sys.modules[package_name] = types.ModuleType(package_name)
+    if jac_machine:
+        jac_machine.loaded_programs[module_name] = module
+        if package_path:
+            parts = package_path.split(".")
+            for i in range(len(parts)):
+                package_name = ".".join(parts[: i + 1])
+                if package_name not in jac_machine.loaded_programs:
+                    jac_machine.loaded_programs[package_name] = types.ModuleType(
+                        package_name
+                    )
 
-        setattr(sys.modules[package_path], module_name, module)
-        sys.modules[f"{package_path}.{module_name}"] = module
-    sys.modules[module_name] = module
+            setattr(jac_machine.loaded_programs[package_path], module_name, module)
+            jac_machine.loaded_programs[f"{package_path}.{module_name}"] = module
     return module
 
 
@@ -187,33 +204,37 @@ def get_caller_dir(target: str, base_path: str, dir_path: str) -> str:
 def py_import(
     target: str,
     caller_dir: str,
-    items: Optional[dict[str, Union[str, bool]]] = None,
+    items: Optional[Dict[str, Union[str, bool]]] = None,
     absorb: bool = False,
     mdl_alias: Optional[str] = None,
+    jac_machine: Optional["JacMachine"] = None,
 ) -> types.ModuleType:
     """Import a Python module."""
     try:
         if target.startswith("."):
+            print(f"Importing {target} from {caller_dir}")
             target = target.lstrip(".")
             full_target = path.normpath(path.join(caller_dir, target))
-            # print(f"Debug: Full target for relative import: {full_target}")
+            print(f"Full target path: {full_target}")
             spec = importlib.util.spec_from_file_location(target, full_target + ".py")
+            print(f"Spec: {spec}")
             if spec and spec.loader:
                 module = importlib.util.module_from_spec(spec)
-                sys.modules[spec.name] = module
                 spec.loader.exec_module(module)
-                # print(f"Debug: Successfully imported relative module: {module}")
+                if jac_machine:
+                    jac_machine.loaded_programs[module.__name__] = module
+                print(f"Module registered: {module.__name__}")
                 return module
             else:
                 raise ImportError(f"Cannot find module {target} at {full_target}")
         else:
+            print(f"Importing {target} as a regular module")
             imported_module = importlib.import_module(name=target)
             main_module = __import__("__main__")
             if absorb:
                 for name in dir(imported_module):
                     if not name.startswith("_"):
                         setattr(main_module, name, getattr(imported_module, name))
-
             elif items:
                 for name, alias in items.items():
                     try:
@@ -237,6 +258,9 @@ def py_import(
                     mdl_alias if isinstance(mdl_alias, str) else target,
                     imported_module,
                 )
+            if jac_machine:
+                jac_machine.loaded_programs[imported_module.__name__] = imported_module
+            print(f"Regular module registered: {imported_module.__name__}")
             return imported_module
     except ImportError as e:
         print(f"Failed to import module {target}: {e}")
@@ -248,7 +272,7 @@ class JacMachine:
 
     def __init__(self) -> None:
         """Initialize a JacMachine instance."""
-        self.loaded_programs: Dict[str, JacProgram] = {}
+        self.loaded_programs: Dict[str, Union[types.ModuleType, JacProgram]] = {}
 
     def load_program(
         self,
@@ -258,10 +282,10 @@ class JacMachine:
         override_name: Optional[str] = None,
         mod_bundle: Optional[Module] = None,
         lng: Optional[str] = "jac",
-        items: Optional[dict[str, Union[str, bool]]] = None,
+        items: Optional[Dict[str, Union[str, bool]]] = None,
         absorb: bool = False,
         mdl_alias: Optional[str] = None,
-    ) -> JacProgram:
+    ) -> Union[types.ModuleType, JacProgram]:
         """Load a Jac program, or return the already loaded instance.
 
         Args:
@@ -271,17 +295,19 @@ class JacMachine:
             override_name (Optional[str], optional): An optional name override for the module. Defaults to None.
             mod_bundle (Optional[Module], optional): An optional module bundle. Defaults to None.
             lng (Optional[str], optional): The language of the module. Defaults to "jac".
-            items (Optional[dict[str, Union[str, bool]]]): Optional items to import.
+            items (Optional[Dict[str, Union[str, bool]]]): Optional items to import.
             absorb (bool): Whether to absorb the imported module into the main module.
             mdl_alias (Optional[str]): Optional alias for the module.
 
         Returns:
-            JacProgram: The loaded Jac program.
+            Union[types.ModuleType, JacProgram]: The loaded Jac program or Python module.
         """
         module_name = path.splitext(path.basename(filename))[0]
         if module_name in self.loaded_programs:
+            print(f"Module {module_name} already loaded.")
             return self.loaded_programs[module_name]
 
+        # Create and initialize JacProgram, which will handle loading
         program = JacProgram(
             filename=filename,
             base_path=base_path,
@@ -292,9 +318,14 @@ class JacMachine:
             items=items,
             absorb=absorb,
             mdl_alias=mdl_alias,
+            jac_machine=self,
         )
-        self.loaded_programs[module_name] = program
-        return program
+        print(f"loaded programs: {self.loaded_programs}")
+        # Return the loaded program
+        if program.module:
+            return program.module
+        else:
+            return program
 
 
 machine = JacMachine()
