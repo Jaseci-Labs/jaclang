e4dba29806590664622ec086e4b7d30284190c1d
mgtm98 <m.g.t.m.prog@gmail.com>
2024-06-29 18:38:41 +0300

    Ignoring typing module to save some time, Fixing module path to point to the stub file instead of the .so file

--- jaclang/compiler/passes/main/py_sym_tab_build_pass.py
+++ jaclang/compiler/passes/main/py_sym_tab_build_pass.py
@@ -6,28 +6,38 @@ for globals, imports, architypes, and abilities declarations and definitions.
 
 import importlib
 import inspect
+import os
 
 import jaclang.compiler.absyntree as ast
 from jaclang.compiler.passes import Pass
 from jaclang.compiler.passes.main import JacImportPass
 from jaclang.compiler.symtable import PySymbolTable, SymbolTable
+from jaclang.settings import settings
 
 
 class PySymTabBuildPass(Pass):
     """Jac Symbol table build pass for python imports and types."""
 
+    def __debug_print(self, *args) -> None:
+        if settings.py_symtab_debug:
+            print("PySymTabBuildPass:", *args)
+
     def push_scope(self, name: str, key_node: ast.AstNode) -> None:
         """Push scope."""
-        print(
+        self.__debug_print(
             f'Adding new scope "{name}", in side the current scope "{self.cur_scope.sym_dotted_name}"'
         )
         self.cur_sym_tab.append(self.cur_scope.push_py_scope(name, key_node))
-        print(f"Moving current scope to be {self.cur_scope.sym_dotted_name}")
+        self.__debug_print(
+            f"Moving current scope to be {self.cur_scope.sym_dotted_name}"
+        )
 
     def pop_scope(self) -> SymbolTable:
         """Pop scope."""
         out = self.cur_sym_tab.pop()
-        print(f"Moving current scope to be {self.cur_scope.sym_dotted_name}")
+        self.__debug_print(
+            f"Moving current scope to be {self.cur_scope.sym_dotted_name}"
+        )
         return out
 
     def to_scope(self, scope_name: str) -> SymbolTable | None:
@@ -35,7 +45,9 @@ class PySymTabBuildPass(Pass):
         scope = self.cur_scope.find_scope(scope_name)
         if scope:
             self.cur_sym_tab.append(scope)
-            print(f"Moving current scope to be {self.cur_scope.sym_dotted_name}")
+            self.__debug_print(
+                f"Moving current scope to be {self.cur_scope.sym_dotted_name}"
+            )
         return scope
 
     def to_main_scope(self) -> None:
@@ -56,6 +68,7 @@ class PySymTabBuildPass(Pass):
 
     def before_pass(self) -> None:
         """Before pass."""
+        super().before_pass()
         self.cur_sym_tab: list[SymbolTable] = [self.ir.sym_tab]
         py_mods = list(JacImportPass.python_modules_to_import)
         py_mods.sort()
@@ -103,14 +116,19 @@ class PySymTabBuildPass(Pass):
         # The top scope should be jac file name and for each module we import
         # symbols from, make sure to have the correct scope
 
+        # Ignoring typing module save us 80% of time
+        if py_mod_name.startswith("typing"):
+            return
+
         while (
             self.cur_scope.name != self.ir.sym_tab.name
             and self.cur_scope.name not in py_mod_name
         ):
             self.pop_scope()
 
-        print("**** Importing data to that scope ****")
+        self.__debug_print("**** Importing data to that scope ****")
         symbol_list = self.get_all_symbols(py_mod_name)
+
         if len(symbol_list) > 0:
             self.push_scope(py_mod_name.split(".")[-1], self.ir)
 
@@ -128,7 +146,7 @@ class PySymTabBuildPass(Pass):
                     0,
                 )
             )
-        print("**************************************")
+        self.__debug_print("**************************************")
 
     def get_all_symbols(self, py_name: str) -> list[dict]:
         """Get all the symbols from a python module.
@@ -154,7 +172,7 @@ class PySymTabBuildPass(Pass):
             - If a submodule or symbol cannot be found, or if introspection fails, empty lists
             or default values are returned for affected symbols.
         """
-        print(f"Trying to import {py_name}")
+        self.__debug_print(f"Trying to import {py_name}")
         out = []
         py_name_lists = py_name.split(".")
         obj = importlib.import_module(py_name_lists[0])
@@ -162,27 +180,29 @@ class PySymTabBuildPass(Pass):
 
         for i in py_name_lists[1:]:
             if stop_importing:
-                print(f"Cannot resolve the name {py_name_lists}, check this!!!")
+                self.__debug_print(
+                    f"Cannot resolve the name {py_name_lists}, check this!!!"
+                )
                 return []
 
             next_obj_fname = ".".join(py_name_lists[: py_name_lists.index(i) + 1])
             parent_obj_fname = ".".join(py_name_lists[: py_name_lists.index(i)])
 
             if not hasattr(obj, i):
-                print(f"Cannot find {i} inside {parent_obj_fname}")
+                self.__debug_print(f"Cannot find {i} inside {parent_obj_fname}")
                 return []
             obj = getattr(obj, i)
 
             if inspect.ismodule(obj):
-                print(f"Found module {i}, importing {next_obj_fname}")
+                self.__debug_print(f"Found module {i}, importing {next_obj_fname}")
                 obj = importlib.import_module(next_obj_fname)
             else:
                 stop_importing = True
 
-        print(f"Getting Symbols from {py_name}, type={type(obj)}")
+        self.__debug_print(f"Getting Symbols from {py_name}, type={type(obj)}")
 
         if isinstance(obj, (int, float, bool)):
-            print("No need to get symbols from that object")
+            self.__debug_print("No need to get symbols from that object")
             return []
 
         for name, member in inspect.getmembers(obj):
@@ -194,7 +214,19 @@ class PySymTabBuildPass(Pass):
             }
 
             try:
-                member_info["file"] = inspect.getfile(member)
+
+                # Extracting python file, stub module if it's a .so file
+                orig_file = inspect.getfile(member)
+                if orig_file.endswith(".so"):
+                    dir_path = orig_file[: orig_file.rindex("/")]
+                    f = orig_file.replace(dir_path, "")
+                    f = f[: f.index(".")]
+                    if os.path.isfile(dir_path + f + ".pyi"):
+                        orig_file = dir_path + f + ".pyi"
+                    elif os.path.isfile(dir_path + f + ".py"):
+                        orig_file = dir_path + f + ".py"
+
+                member_info["file"] = orig_file
                 lines = inspect.getsourcelines(member)
                 member_info["start_line"] = lines[1]
                 member_info["end_line"] = lines[1] + 1
