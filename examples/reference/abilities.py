63240619f69628471170efb2d2df8af0bfbbed5e
TharukaCKasthuri <tharuka.ckasthuri@gmail.com>
2024-07-19 17:18:27 +0200

    Jaclang abilities example update

--- examples/reference/abilities.py
+++ examples/reference/abilities.py
@@ -1,32 +1,116 @@
 from abc import ABC, abstractmethod
 
-
 class Calculator(ABC):
-    @staticmethod
-    def multiply(a: float, b: float) -> float:
-        return a * b
+    """
+    Abstract Base Class for basic calculator operations.
+    """
 
     @abstractmethod
-    def substract(self, x: float, y: float) -> float:
+    def add(self, x: float, y: float) -> float:
+        """
+        Add two numbers.
+
+        :param x: First number
+        :param y: Second number
+        :return: The sum of x and y
+        """
         pass
 
-    def add(self, number: float, *a: tuple) -> str:
-        return str(number * sum(a))
+    @abstractmethod
+    def subtract(self, x: float, y: float) -> float:
+        """
+        Subtract two numbers.
+
+        :param x: First number
+        :param y: Second number
+        :return: The difference of x and y
+        """
+        pass
+
+    @abstractmethod
+    def multiply(self, x: float, y: float) -> float:
+        """
+        Multiply two numbers.
+
+        :param x: First number
+        :param y: Second number
+        :return: The product of x and y
+        """
+        pass
+
+    @abstractmethod
+    def divide(self, x: float, y: float) -> float:
+        """
+        Divide two numbers.
+
+        :param x: First number
+        :param y: Second number
+        :return: The quotient of x and y
+        """
+        pass
 
 
-class Substractor(Calculator):
-    def substract(self, x: float, y: float) -> float:
+class BasicCalculator(Calculator):
+    """
+    Basic calculator implementation.
+    """
+
+    def add(self, x: float, y: float) -> float:
+        return x + y
+
+    def subtract(self, x: float, y: float) -> float:
         return x - y
 
+    def multiply(self, x: float, y: float) -> float:
+        return x * y
 
-class Divider:
-    def divide(self, x: float, y: float):
+    def divide(self, x: float, y: float) -> float:
         return x / y
+    
+class AdvanceCalculator(BasicCalculator):
+    """
+    Advance calculator implementation.
+    """
+
+    def power(self, x: float, y: float) -> float:
+        """
+        Raise x to the power of y.
+        """
+        return x ** y
+
+    def square_root(self, x: float) -> float:
+        """
+        Compute the square root of x.
+        """
+        return x ** 0.5
+
+    def cube_root(self, x: float) -> float:
+        """
+        Compute the cube root of x.
+        """
+        return x ** (1/3)
+
+    def remainder(self, x: float, y: float) -> float:
+        """
+        Compute the remainder of x divided by y.
+        """
+        return x % y
+
+    def absolute(self, x: float) -> float:
+        """
+        Compute the absolute value of x.
+        """
+        return abs(x)
 
 
-sub = Substractor()
-div = Divider()
-print(div.divide(55, 11))
-print(Calculator.multiply(9, -2))
-print(sub.add(5, 20, 34, 56))
-print(sub.substract(9, -2))
+cal = BasicCalculator()
+adv_cal = AdvanceCalculator()
+print(cal.add(5, 10))
+print(cal.subtract(5, 10))
+print(cal.multiply(5, 10))
+print(cal.divide(5, 10))
+print(adv_cal.power(5, 10))
+print(adv_cal.square_root(25))
+print(adv_cal.cube_root(27))
+print(adv_cal.remainder(10, 3))
+print(adv_cal.absolute(-5))
